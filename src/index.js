ef8ad44a0787e41f3c4739ba40cebe7af3e185eb
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2024-08-06 15:50:00 +0200

    Fix wrong check

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ const getTextNodesInElementContainingText = (element, text) => {
 const getRangesForSearchTermInElement = (element, search) => {
   const ranges = [];
   const lowerCaseSearch = search.toLowerCase();
-  if (!element.childNodes.length === 0) return ranges;
+  if (element.childNodes.length === 0) return ranges;
   // In some frameworks like React, when combining static text with dynamic text, the element may have multiple Text child nodes.
   // To avoid errors, we must find the child node that actually contains the search term.
   const childWithSearchTerm = Array.from(element.childNodes).find((node) =>
