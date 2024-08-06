4304d0ada9397e99606a640d568f41a528933b10
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2024-08-06 15:29:33 +0200

    Fix the issue with combinations of static and dynamic texts

--- src/index.js
+++ src/index.js
@@ -37,6 +37,10 @@ const highlightSearchTerm = ({
       });
     });
   } catch (error) {
+    // When combining static text with dynamic text in React, the textContent property may be different from the wholeText property.
+    // wholeText will contains both static and dynamic text, while textContent will only contain the static text.
+    // The Range object in chromium browsers seems to be using the textContent and it could throw
+    // an error when we did detect the search term.
     console.error(error);
   }
   if (ranges.length === 0) return;
@@ -61,14 +65,20 @@ const getTextNodesInElementContainingText = (element, text) => {
 const getRangesForSearchTermInElement = (element, search) => {
   const ranges = [];
   const lowerCaseSearch = search.toLowerCase();
-  if (!element.firstChild) return ranges;
-  const text = element.textContent?.toLowerCase() || "";
+  if (!element.childNodes.length === 0) return ranges;
+  // In some frameworks like React, when combining static text with dynamic text, the element may have multiple Text child nodes.
+  // To avoid errors, we must find the child node that actually contains the search term.
+  const childWithSearchTerm = Array.from(element.childNodes).find((node) =>
+    node.textContent?.toLowerCase().includes(lowerCaseSearch)
+  );
+  if (!childWithSearchTerm) return ranges;
+  const text = childWithSearchTerm.textContent?.toLowerCase() || "";
   let start = 0;
   let index;
   while ((index = text.indexOf(lowerCaseSearch, start)) >= 0) {
     const range = new Range();
-    range.setStart(element.firstChild, index);
-    range.setEnd(element.firstChild, index + search.length);
+    range.setStart(childWithSearchTerm, index);
+    range.setEnd(childWithSearchTerm, index + search.length);
     ranges.push(range);
     start = index + search.length;
   }
